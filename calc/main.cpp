d592aa1272f7e8ac43d2857d65fcf4bc68fd855d
shkabarev <shkabarev@IT8>
2024-12-10 17:00:36 +0500

    Add project files.

---
+++ calc/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+
+void main()
+{
+	
+	setlocale(LC_ALL, "");
+
+	string entered_expression;
+
+	cout << "¬ведите выражение: "; cin >> entered_expression;
+
+
+
+
+}
